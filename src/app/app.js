5dcff82966db349cb6a750801d6627c8e9f14fce
kuus <kunderikuus@gmail.com>
2014-09-20 01:17:59 +0200

    jshint

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,7 @@
 (function(window) {
 
   // useful vars which access parent page, including local jquery
-  var windowParent = window.parent
+  var windowParent = window.parent;
   var documentParent = window.parent.document;
   var $ = windowParent.jQuery;
 
